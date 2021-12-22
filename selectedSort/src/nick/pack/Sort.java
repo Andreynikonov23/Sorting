68b2fe4bde75c0944a26a0b77b797400fc2d3b0a
Andreynikonov23 <76613379+Andreynikonov23@users.noreply.github.com>
2021-12-22 14:06:02 +0300

    Add files via upload

---
+++ selectedSort/src/nick/pack/Sort.java
@@ -0,0 +1,26 @@
+package nick.pack;
+
+import java.util.Arrays;
+
+public class Sort {
+    public static void main(String[] args) {
+        /*
+        Каждый проход выбирать самый минимальный элемент и смещать его в начало.
+        При этом каждый новый проход начинать сдвигаясь вправо,
+        то есть первый проход — с первого элемента, второй проход — со второго.
+         */
+        int[] array = {10,2,5,23,124,14,5,1,6,33};
+        for (int i = 0; i < array.length -1; i++) {
+            int least = i;
+            for (int j = i+1; j < array.length; j++) {
+                if(array[j] < array[least]){
+                    least = j;
+                }
+            }
+            int tmp = array[i];
+            array[i] = array[least];
+            array[least] = tmp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
